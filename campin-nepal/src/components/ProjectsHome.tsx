385a6ee84819228655b3b922e8048affd7f818c9
Bishow Adhikari <bishow712@gmail.com>
2024-02-21 17:10:51 +0545

    Products

--- campin-nepal/src/components/ProjectsHome.tsx
+++ campin-nepal/src/components/ProjectsHome.tsx
@@ -24,17 +24,27 @@ const ProjectsHome = () => {
         }
     };
 
+    const products = [
+        { image: about1, name: "tent", description: "tent" },
+        { image: about1, name: "tent-2", description: "tent-2" },
+        { image: about1, name: "tent-2", description: "tent-2" },
+        { image: about1, name: "tent-2", description: "tent-2" },
+        { image: about1, name: "tent-2", description: "tent-2" },
+        { image: about1, name: "tent-2", description: "tent-2" },
+
+    ]
+
     return (
         <div className='bg-[#1e2d37]'>
             <div className='products__header'>
-            <h1 className='products__header-1 text-center'>Our Products</h1>
+                <h1 className='products__header-1 text-center'>Our Products</h1>
             </div>
 
             <div className="viweallprojects__btn">
-              <a href="#">
-                View all Products
-                <span><i className="ri-arrow-right-line"></i></span>
-              </a>
+                <a href="#">
+                    View all Products
+                    <span><i className="ri-arrow-right-line"></i></span>
+                </a>
             </div>
 
             <div className='flex flex-wrap justify-center p-4'>
@@ -52,54 +62,15 @@ const ProjectsHome = () => {
                     containerClass="carousel-container"
                     itemClass="carousel-items">
 
-                    <a href='#' target='_blank' rel="noreferrer"
-                        className="relative isolate h-96 flex flex-col justify-end overflow-hidden rounded-2xl px-4 pb-4 pt-40 m-4 hover:scale-110 transition-transform duration-200 ease-in-out">
-                        <img src={`${about1}`} alt="" className="absolute inset-0 h-full w-full object-cover" />
-                        <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/90"></div>
-                        <h3 className="z-10 mt-3 text-3xl font-bold text-[#4FC1E9]">Camping Tent</h3>
-                        <div className="z-10 overflow-hidden pt-2 text-sm font-bold text-gray-500">Camping Tent</div>
-                    </a>
-
-                    <a href='#' target='_blank' rel="noreferrer"
-                        className="relative isolate h-96 flex flex-col justify-end overflow-hidden rounded-2xl px-4 pb-4 pt-40 m-4 hover:scale-110 transition-transform duration-200 ease-in-out">
-                        <img src={`${about1}`} alt="" className="absolute inset-0 h-full w-full object-cover" />
-                        <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/90"></div>
-                        <h3 className="z-10 mt-3 text-3xl font-bold text-[#4FC1E9]">Camping Tent</h3>
-                        <div className="z-10 overflow-hidden pt-2 text-sm font-bold text-gray-500">Camping Tent</div>
-                    </a>
-
-                    <a href='#' target='_blank' rel="noreferrer"
-                        className="relative isolate h-96 flex flex-col justify-end overflow-hidden rounded-2xl px-4 pb-4 pt-40 m-4 hover:scale-110 transition-transform duration-200 ease-in-out">
-                        <img src={`${about1}`} alt="" className="absolute inset-0 h-full w-full object-cover" />
-                        <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/90"></div>
-                        <h3 className="z-10 mt-3 text-3xl font-bold text-[#4FC1E9]">Camping Tent</h3>
-                        <div className="z-10 overflow-hidden pt-2 text-sm font-bold text-gray-500">Camping Tent</div>
-                    </a>
-
-                    <a href='#' target='_blank' rel="noreferrer"
-                        className="relative isolate h-96 flex flex-col justify-end overflow-hidden rounded-2xl px-4 pb-4 pt-40 m-4 hover:scale-110 transition-transform duration-200 ease-in-out">
-                        <img src={`${about1}`} alt="" className="absolute inset-0 h-full w-full object-cover" />
-                        <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/90"></div>
-                        <h3 className="z-10 mt-3 text-3xl font-bold text-[#4FC1E9]">Camping Tent</h3>
-                        <div className="z-10 overflow-hidden pt-2 text-sm font-bold text-gray-500">Camping Tent</div>
-                    </a>
-
-                    <a href='#' target='_blank' rel="noreferrer"
-                        className="relative isolate h-96 flex flex-col justify-end overflow-hidden rounded-2xl px-4 pb-4 pt-40 m-4 hover:scale-110 transition-transform duration-200 ease-in-out">
-                        <img src={`${about1}`} alt="" className="absolute inset-0 h-full w-full object-cover" />
-                        <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/90"></div>
-                        <h3 className="z-10 mt-3 text-3xl font-bold text-[#4FC1E9]">Camping Tent</h3>
-                        <p className="z-10 overflow-hidden pt-2 text-sm font-bold text-gray-500">Camping Tent</p>
-                    </a>
-
-                    <a href='#' target='_blank' rel="noreferrer"
-                        className="relative isolate h-96 flex flex-col justify-end overflow-hidden rounded-2xl px-4 pb-4 pt-40 m-4 hover:scale-110 transition-transform duration-200 ease-in-out">
-                        <img src={`${about1}`} alt="" className="absolute inset-0 h-full w-full object-cover" />
-                        <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/90"></div>
-                        <h3 className="z-10 mt-3 text-3xl font-bold text-[#4FC1E9]">Camping Tent</h3>
-                        <div className="z-10 overflow-hidden pt-2 text-sm font-bold text-gray-500">Camping Tent</div>
-                    </a>
-
+                    {products.map((m, index) => (
+                        <a key={index} href='#' target='_blank' rel="noreferrer"
+                            className="relative isolate h-96 flex flex-col justify-end overflow-hidden rounded-2xl px-4 pb-4 pt-40 m-4 hover:scale-110 transition-transform duration-200 ease-in-out">
+                            <img src={m.image} alt="" className="absolute inset-0 h-full w-full object-cover" />
+                            <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/90"></div>
+                            <h3 className="z-10 mt-3 text-3xl font-bold text-[#4FC1E9]">{m.name}</h3>
+                            <div className="z-10 overflow-hidden pt-2 text-sm font-bold text-gray-500">{m.description}</div>
+                        </a>
+                    ))}
                 </Carousel>
             </div>
         </div>
